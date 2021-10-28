9946b05352afafd3241e8b9121b3019e5d5084ca
talhadengiz <talhadengiz@gmail.com>
2021-10-28 21:26:59 +0300

    Extension function added for date format.

--- app/src/main/java/com/talhadengiz/hepsiburada/data/adapter/SearchRecyclerViewAdapter.kt
+++ app/src/main/java/com/talhadengiz/hepsiburada/data/adapter/SearchRecyclerViewAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.talhadengiz.hepsiburada.data.model.DataResponse
 import com.talhadengiz.hepsiburada.data.model.Result
 import com.talhadengiz.hepsiburada.databinding.ItemRecyclerviewBinding
+import com.talhadengiz.hepsiburada.util.convertToDateFormat
 import com.talhadengiz.hepsiburada.util.downloadFromUrl
 import com.talhadengiz.hepsiburada.util.placeHolderProgressBar
 
@@ -32,7 +33,7 @@ class SearchRecyclerViewAdapter :
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
         holder.binding.tvCollectionName.text = resultList?.get(position)?.collectionName
         holder.binding.tvCollectionPrice.text = resultList?.get(position)?.collectionPrice.toString()
-        holder.binding.tvReleaseDate.text = resultList?.get(position)?.releaseDate
+        holder.binding.tvReleaseDate.text = resultList?.get(position)?.releaseDate?.convertToDateFormat("yyyy-MM-dd'T'HH:mm:sss","dd.MM.yyyy")
 
         holder.binding.ivItem.downloadFromUrl(
             resultList?.get(position)?.artworkUrl100,

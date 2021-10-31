862896c0738d69bb18f6f155ac0ea8be64a1dbfa
talhadengiz <talhadengiz@gmail.com>
2021-10-31 10:55:00 +0300

    Splash screen added.

--- app/src/main/java/com/talhadengiz/hepsiburada/ui/activity/MainActivity.kt
+++ app/src/main/java/com/talhadengiz/hepsiburada/ui/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import com.talhadengiz.hepsiburada.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Hepsiburada)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
